42142490e97a96031f3f1500eb00ea979ae10783
ospatrycja <patrycjajoannaosinska@gmail.com>
2022-03-22 16:33:16 +0100

    [ADD] P2022-1890 - setup requests logeger

--- src/test/java/com/patronage/backend/BaseTest.java
+++ src/test/java/com/patronage/backend/BaseTest.java
@@ -1,5 +1,33 @@
 package com.patronage.backend;
 
+import io.restassured.RestAssured;
+import io.restassured.filter.log.ErrorLoggingFilter;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+
 public class BaseTest extends Endpoints {
 
+    @Rule
+    public TestName testName = new TestName();
+
+    @BeforeClass
+    public static void setupRestAssuredLogging() {
+        RestAssured.filters(
+                new RequestLoggingFilter(),
+                new ResponseLoggingFilter(),
+                new ErrorLoggingFilter()
+        );
+    }
+
+    @Before
+    public void logTestName() {
+        System.out.println("--------------------------------------------------");
+        System.out.println("RUNNING TEST:");
+        System.out.println(testName.getMethodName());
+        System.out.println("--------------------------------------------------");
+    }
 }
