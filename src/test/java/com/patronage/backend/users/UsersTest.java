d7d63d44bd8a66c2ea89e63ab834915aea1a1d41
LAPTOP-V2N4JIGA\Gathaspar66 <lukasiewiczkacper@wp.pl>
2022-04-06 21:02:25 +0200

    P2022-1724-Automate Add new tests

--- src/test/java/com/patronage/backend/users/UsersTest.java
+++ src/test/java/com/patronage/backend/users/UsersTest.java
@@ -2,6 +2,8 @@ package com.patronage.backend.users;
 
 import com.patronage.backend.Endpoints;
 import com.patronage.backend.auth.BaseAuthTest;
+import io.restassured.specification.RequestSpecification;
+import org.apache.http.HttpStatus;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,17 +12,35 @@ import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.apache.http.HttpHeaders.AUTHORIZATION;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class UsersTest extends BaseAuthTest {
 
-    @Test
-    public void postAuthorizationDataShouldReturnToken() {
-
+    private String getAuthorizationToken() {
         Map<String, String> params = new HashMap<>();
         params.put("email", System.getenv("USER_EMAIL"));
         params.put("password", System.getenv("USER_PASSWORD"));
-        String token = given().when().queryParams(params).post(Endpoints.LOGIN).getHeader(AUTHORIZATION);
+        return given().when().queryParams(params).post(Endpoints.LOGIN).getHeader(AUTHORIZATION);
+    }
 
-        Assert.assertNotNull(token);
+    @Test
+    public void postAuthorizationDataShouldReturnToken() {
+        Assert.assertNotNull(getAuthorizationToken());
+    }
+
+    @Test
+    public void postCreatingBoardRequestShouldReturnStatusCode201() {
+        RequestSpecification header = given().header(AUTHORIZATION, getAuthorizationToken());
+        givenBodyJson(header, "{\"name\": \"name77\"}")
+                .when().post(Endpoints.BOARDS)
+                .then().statusCode(HttpStatus.SC_CREATED);
+    }
+
+    @Test
+    public void postCreatingBoardRequestShouldReturnID() {
+        RequestSpecification header = given().header(AUTHORIZATION, getAuthorizationToken());
+        givenBodyJson(header, "{\"name\": \"name77\"}")
+                .when().post(Endpoints.BOARDS)
+                .then().body("id", notNullValue());
     }
 }
